757536bf13c73c7718382ad53edb72afc43e048e
LIstaTimes <ryllarygfs@gmail.com>
2024-04-02 22:16:47 -0300

    sofrendo

--- app/src/main/java/br/com/myskout/bottomnavigationbar/ListaTimesAdapter.java
+++ app/src/main/java/br/com/myskout/bottomnavigationbar/ListaTimesAdapter.java
@@ -3,22 +3,61 @@ package br.com.myskout.bottomnavigationbar;
 import android.content.Context;
 import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.cardview.widget.CardView;
+import androidx.recyclerview.widget.RecyclerView;
 
 import java.util.List;
 
-public class ListaTimesAdapter {
+public class ListaTimesAdapter extends RecyclerView.Adapter<ListaTimesAdapter.ViewHolder> {
     private List<Times> lstTimes;
     private Context myContext;
 
-    public ListaTimesAdapter(List<Times> lstTimes, Context myContext) {
+
+    public ListaTimesAdapter(Context myContext, List<Times> lstTimes) {
+        this.myContext = myContext;
+        this.lstTimes = lstTimes;
+    }
+
+    public ListaTimesAdapter(List<Times> lstTimes, Context myContext, ViewGroup parent, int viewType) {
         this.lstTimes = lstTimes;
         this.myContext = myContext;
     }
 
-    public ListaTimesAdapter.ViewHolder onCreateViewHolder(){
-        View view = LayoutInflater.from(myContext).inflate(R.layout.)
+
+    public ListaTimesAdapter.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(myContext).inflate(R.layout.modelo_times, parent, false);
+        return new ViewHolder(view);
     }
 
-    public class ViewHolder {
+    public void onBindViewHolder(ListaTimesAdapter.ViewHolder holder, int position) {
+        Times times = lstTimes.get(position);
+        holder.txtNomeTimes.setText(times.getNome());
+        holder.imgTimes.setImageResource(times.getImage());
+        holder.cardTimes.setOnClickListener(view -> Toast.makeText(myContext, "teams", Toast.LENGTH_SHORT).show());
+
+    }
+
+    public int getItemCount() {
+        return lstTimes.size();
+
+        public static class ViewHolder extends RecyclerView.ViewHolder {
+            TextView txtNomeTimes;
+            ImageView imgTimes;
+            TextView txtTitulos;
+            CardView cardTimes;
+
+            public ViewHolder(View itemView) {
+                super(itemView);
+                txtNomeTimes = itemView.findViewById(R.id.id_lblNomeTimes);
+                imgTimes = itemView.findViewById(R.id.id_imgTimes);
+                txtTitulos = itemView.findViewById(R.id.id_lblTitulos);
+                cardTimes = itemView.findViewById(R.id.id_CardView);
+            }
+        }
     }
 }
--- app/src/main/java/br/com/myskout/bottomnavigationbar/MainActivity.java
+++ app/src/main/java/br/com/myskout/bottomnavigationbar/MainActivity.java
@@ -2,6 +2,8 @@ package br.com.myskout.bottomnavigationbar;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.GridLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 import android.view.MenuItem;
@@ -10,10 +12,16 @@ import com.google.android.material.badge.BadgeDrawable;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.google.android.material.navigation.NavigationBarView;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class MainActivity extends AppCompatActivity {
     BottomNavigationView bottomNavigationView;
 
     HomeFragment homeFragment = new HomeFragment();
+    RecyclerView RecycleViewTimes;
+    List<Times> lstTimes;
+
     NotificationsFragment notificationsFragment = new NotificationsFragment();
     SettingsFragment settingsFragment = new SettingsFragment();
 
@@ -39,6 +47,14 @@ public class MainActivity extends AppCompatActivity {
                         getSupportFragmentManager().
                                 beginTransaction().
                                 replace(R.id.frmContainer, homeFragment).commit();
+
+                        lstTimes = new ArrayList<>();
+                        lstTimes.add(new Times(("Corinthians", R.drawable.corinthians, 1));
+
+                        RecycleViewTimes = findViewById(R.id.idRecyclerTimes);
+                        RecycleViewTimes.setLayoutManager(new GridLayoutManager(getApplicationContext(), 2));
+                        ListaTimesAdapter adapter = new ListaTimesAdapter(getApplicationContext(),lstTimes);
+                        RecycleViewTimes.setAdapter(adapter);
                         return true;
                     case R.id.mNotifications:
                         getSupportFragmentManager().
